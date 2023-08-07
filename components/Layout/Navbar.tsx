6113ae88f416517201bf41438f3ca992f107e8a0
Flocqst <flocastet@icloud.com>
2023-08-07 15:14:36 +0200

    App structure & basic navbar

---
+++ components/Layout/Navbar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Image from "next/image";
+
+function Navbar() {
+    return (
+    <nav className="fixed top-0 z-30 fflex-no-wrap relative flex w-full items-center justify-between bg-orange-100 px-6 py-3">
+        <div className="flex w-full flex-wrap items-center justify-between px-4">
+            <ul className="list-style-none mr-auto flex flex-col pl-0 lg:flex-row">
+                <li className="mb-4 lg:mb-0 lg:pr-8">
+                    <Link href="/" className="flex items-center gap-2">
+                        <Image src="/vertex.svg" alt="logo" width={30} height={30} />
+                        <p className="font-mono text-slate-900 max-xs:hidden">Vertex</p>
+                    </Link>
+                </li>
+                <li className="mb-4 lg:mb-0 lg:pr-4">
+                    <Link href="/overview" className="flex items-center gap-2">
+                        <p className="text-neutral-600 transition duration-200 hover:text-neutral-900 hover:ease-in-out focus:text-neutral-600 motion-reduce:transition-none">Overview</p>
+                    </Link>
+                </li>
+                <li className="mb-4 lg:mb-0 lg:pr-4">
+                    <Link href="/futures" className="flex items-center gap-2">
+                        <p className="text-neutral-600 transition duration-200 hover:text-neutral-900 hover:ease-in-out focus:text-neutral-600 motion-reduce:transition-none">Futures</p>
+                    </Link>
+                </li>
+                <li className="mb-4 lg:mb-0 lg:pr-4">
+                    <Link href="/docs" className="flex items-center gap-2">
+                        <p className="text-neutral-600 transition duration-200 hover:text-neutral-900 hover:ease-in-out focus:text-neutral-600 motion-reduce:transition-none">Docs</p>
+                    </Link>
+                </li>
+            </ul>
+
+        </div>
+        <div className="relative flex items-center">
+            <p className="text-neutral-500">Connect</p>
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar;
